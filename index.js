15187230cfa9c644be5d5bbce79b2bfcc895b367
ahn0ng <71095151+ahn0ng@users.noreply.github.com>
2020-10-14 02:03:15 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+class Fox {
+
+    constructor(){
+      this.name = 'Foxible';
+      this.home = 'fox town';
+      this.friends = [none];
+      this.seeking = ['dream', 'challenge'];
+     
+  
+    }
+  
+
+    fight(){
+      //he will figth with giveup
+      if (meetgiveup) {
+        attack (withfireball)
+      }
+    }
+
+    seekchallenge(){
+      //He believe he can do anything
+     
+    }
+    
+    cry(){
+      //crys when failed
+       }
+  
+  }
